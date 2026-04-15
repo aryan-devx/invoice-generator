ed0ea76fda6166d0b539143df621a87744d34fb7
Aryan <aryankumar91237@gmail.com>
2026-04-16 00:20:02 +0530

    backend connect

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://invoice-generator-production-6829.up.railway.app",
         changeOrigin: true,
       },
     },
